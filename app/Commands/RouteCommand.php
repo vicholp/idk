a2211ea60d01eab6063516520ee70d844ebed369
vicholpe <vlinerospardo@gmail.com>
2022-09-17 04:53:28 -0300

    feat: small improvement for list routes

--- app/Commands/RouteCommand.php
+++ app/Commands/RouteCommand.php
@@ -9,8 +9,9 @@ class RouteCommand
     public function list(): void
     {
         $routes = (new Router())->getRoutes();
+
         foreach ($routes as $route) {
-            echo $route->getPath()."\n";
+            dump($route->getPath() . ' -> ' . $route->getDefaults()['_controller'] . '::' .$route->getDefaults()['_action']);
         }
     }
 }
